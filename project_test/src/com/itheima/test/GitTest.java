5f1e6b03483d9345a4d90defe4ef9e68475fecbf
mygod <itheima20181014@163.com>
2019-02-16 21:09:18 +0800

    将工程添加到本地仓库

---
+++ project_test/src/com/itheima/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.itheima.test;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git test");
+        System.out.println("hello git test");
+        System.out.println("hello git test");
+        System.out.println("hello git test");
+        System.out.println("hello git test");
+        System.out.println("hello git test");
+        System.out.println("hello git test");
+    }
+
+}
