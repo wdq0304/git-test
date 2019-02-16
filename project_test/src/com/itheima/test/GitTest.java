7b9802456da878c556e8271336ff3ad9c752475f
mygod <itheima20181014@163.com>
2019-02-16 21:13:01 +0800

    对gittest进行了一点修改...

--- project_test/src/com/itheima/test/GitTest.java
+++ project_test/src/com/itheima/test/GitTest.java
@@ -9,6 +9,11 @@ public class GitTest {
         System.out.println("hello git test");
         System.out.println("hello git test");
         System.out.println("hello git test");
+        test();
     }
-
+    public static void test(){
+        int i=1;
+        int b=i+10;
+        System.out.println(b);
+    }
 }
