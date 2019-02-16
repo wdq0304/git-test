abf3868eeaf8270e844751a2e0606ccce2bb90a1
mygod <itheima20181014@163.com>
2019-02-16 21:38:20 +0800

    添加second类文件内容

---
+++ project_test/src/com/itheima/test/Second.java
@@ -0,0 +1,10 @@
+package com.itheima.test;
+
+public class Second {
+    public static void test(){
+        int i=1;
+        int b=i+10;
+        System.out.println(b);
+        System.out.println(b+1);
+    }
+}
