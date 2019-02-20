7f7792de62f7a279869c484a39bea10da431e6cb
mygod <itheima20181014@163.com>
2019-02-20 16:15:42 +0800

    新增First.java文件

---
+++ project_test/src/com/itheima/test/First.java
@@ -0,0 +1,13 @@
+package com.itheima.test;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("nihaoma?");
+        System.out.println("nihaoma?");
+        System.out.println("nihaoma?");
+        System.out.println("nihaoma?");
+        System.out.println("nihaoma?");
+        System.out.println("nihaoma?");
+        System.out.println("nihaoma?");
+    }
+}
